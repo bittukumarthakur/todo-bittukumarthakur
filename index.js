20336ac3289eaefabc665246d8cf74aaf68fcae0
Bittu <bittukumarthakur@users.noreply.github.com>
2023-07-18 17:46:33 +0530

    Extract some task to a separate funtion.

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+const mark = (event) => {
+  const element = event.target;
+  element.classList.add("green");
+};
+
+const createTaskElement = (message) => {
+  const newTask = document.createElement("li");
+  newTask.innerText = message;
+  return newTask;
+};
+
 const addTask = () => {
   const task = document.querySelector("#task-title");
   const list = document.querySelector("#list");
-  const newTask = document.createElement("li");
-  newTask.innerText = task.value;
+
+  const newTask = createTaskElement(task.value);
   list.appendChild(newTask);
 
-  newTask.onclick = () => newTask.classList.add("green");
-
+  newTask.onclick = mark;
 };
 
 const main = () => {
