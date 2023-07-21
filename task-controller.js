a4686eaa5e308ce552fc43e3aa6253098173a299
Bittu <bittukumarthakur@users.noreply.github.com>
2023-07-21 12:40:04 +0530

    refactor:add todo initial template, in task-list-view.

--- task-controller.js
+++ task-controller.js
@@ -1,3 +1,22 @@
 class taskController {
-  constructor()
+  #taskList;
+  #taskListView;
+
+  constructor(taskList, taskListView) {
+    this.#taskList = taskList;
+    this.#taskListView = taskListView;
+  }
+
+  #addTask(description) {
+    this.#taskList.addTask(description);
+  }
+
+  render() {
+    const tasksDetail = taskList.report();
+    this.#taskListView.render(tasksDetail);
+  }
+
+  start() {
+    
+  }
 }
--- task-list-view.js
+++ task-list-view.js
@@ -1,8 +1,29 @@
 class TaskListView {
-  #container;
+  #todoPage;
+  #inputBox;
+  #addButton;
+  #taskListContainer;
 
-  constructor(container) {
-    this.#container = container;
+  constructor() {
+    // we need to move some where else.
+    this.#todoPage = document.createElement("div");
+    this.#todoPage.classList.add("todoPage");
+
+    this.#inputBox = document.createElement("input")
+    this.#inputBox.type = "text";
+    this.#inputBox.placeholder = "Type your task here";
+
+    this.#addButton = document.createElement("input");
+    this.#addButton.type = "button";
+    this.#addButton.value = "Add";
+
+    this.#taskListContainer = document.createElement("ol");
+
+    this.#todoPage.appendChild(this.#inputBox);
+    this.#todoPage.appendChild(this.#addButton);
+    this.#todoPage.appendChild(this.#taskListContainer);
+
+    document.appendChild(this.#todoPage);
   }
 
   #createElement({ id, description, isMarked }) {
@@ -14,17 +35,18 @@ class TaskListView {
     return element;
   }
 
-  #clearContainer() {
-    const elements = [...this.#container.children];
+  #clearTaskListContainer() {
+    const elements = [...this.#taskListContainer.children];
     elements.forEach(element => element.remove());
   }
 
   render(tasksDetail) {
-    this.#clearContainer();
+    this.#clearTaskListContainer();
 
     tasksDetail.forEach((taskDetail) => {
       const taskElement = this.#createElement(taskDetail);
-      this.#container.appendChild(taskElement);
+      this.#taskListContainer.appendChild(taskElement);
     });
   }
+
 };
